60516b6c5dfe1bb9dada0feea421c16fac4cbeda
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-12-22 09:04:03 +0300

    Task_03

---
+++ Folder_3/D.z.py
@@ -0,0 +1,23 @@
+# Сгенерируйте список на 30 элементов. Отсортируйте список по возрастанию, методом сортировки выбором.
+
+from random import randint
+ 
+ 
+def sel_sort(array):
+    for i in range(len(array) - 1):
+        m = i
+        j = i + 1
+        while j < len(array):
+            if array[j] < array[m]:
+                m = j
+            j = j + 1
+        array[i], array[m] = array[m], array[i]
+ 
+ 
+a = []
+for i in range(10):
+    a.append(randint(1, 30))
+ 
+print(a)
+sel_sort(a)
+print(a)
